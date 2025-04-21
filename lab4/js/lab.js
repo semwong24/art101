0a1a3bc2f9c6e19c104c9fbec03dcd97d252c843
semwong24 <serenemwong@gmail.com>
2025-04-21 15:26:02 -0700

    Lab 4 Changes
    The included updates feature code changes to the CSS, JS, and HTML files to complete Lab 4.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Task 3: Turn pseudocode into comments - Dice Game
+// An implementation of the game
+// Author: Serene Wong <semwong@ucsc.edu> (and Elio Kim)
+// Date: 4/21/25
+
+// Dice Game:
+  // Initialize total score to 0
+  // Initialize number of throws to 0
+  // Explain rules to player
+    // Tell player the goal: reach 50 points in no more than 10 turns
+    // After each dice roll, the player selects an operation (+, -, *, /)
+    // A second dice is rolled and the result is used in the chosen operation
+    // The result of the operation is added to the total score
+    // Division will use integer division
+    // The game ends when the player reaches 50 or more points (win) or after 10 turns without reaching 50 (lose)
+  // While total score is less than 50 and number of throws is less than 10:
+    //    Simulate a dice roll (random number between 1 and 6)
+    //    Display result to the player
+    //    Ask the player to choose an operation (+, -, *, /)
+    //    Simulate a second dice roll (random number between 1 and 6)
+    //    Apply the chosen operation to the total score using the second dice roll
+      //    If “+”, add second roll to total
+      //    If “-”, subtract second roll from total
+      //    If “*”, multiply total by second roll
+      //    If “/”, use integer division
+    //    Update and display new total score
+    //    Increase number of throws by 1
+  // If total score is 50 or more:
+    //    Display “You Win!”
+  // Else:
+    //    Display “You Lose! Game Over”
 
 // Constants
 
