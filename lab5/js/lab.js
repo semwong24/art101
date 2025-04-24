d818e3dad1708030e8abad7aa844b419c9cafd1b
semwong24 <serenemwong@gmail.com>
2025-04-23 18:17:27 -0700

    Lab 5 Changes
    Updates here include changes made to complete tasks 2-4 of Lab 5. JSS file is updated with vehicle information, HTML is updated with the necessary information, and the CSS sheet has minor changes for visual clarity on the local page.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/* lab.js - Task 2: A JavaScript Program
+  * Put a comment blockLinks to an external site. at the top of your program
+  * Declare all the variables you created in Task 1 in this JavaScript file
+  * Make it neat using plenty of whitespace.
+  * Style in programming matters, so use indentations and spaces where necessary.
+  * Remember to put a semicolon at the end of every line.
+  * Use document.writeln() to neatly output each of your variables, example,
+* Author: Serene Wong
+* Date: 4/23/25
+* License: Public Domain
+*/
 
-// Constants
+// Define Variables
+make = "Toyota";
+model = "RAV4";
+color = "Blue";
+year = 2007;
+ownIt = false;
+currentYear = 2025;
 
-// Functions
+// calculate
+let age = currentYear - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Owned: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
