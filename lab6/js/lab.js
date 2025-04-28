d3569712d884d1f87cd8853e69aa800765f4313c
semwong24 <serenemwong@gmail.com>
2025-04-27 20:55:48 -0700

    Lab 6 Changes
    Changes to the code made here complete the necessary tasks for Lab 6. The JS file was updated with the array and object variables, which were then called in the HTML file. CSS was also called to help with the styling of the completed page.

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - Lab 6: Arrays and Objects for Transportation
+// Author: Serene Wong <semwong@ucsc.edu>
+// Date: 4/27/25
 
-// Constants
+// Define Variables
+let myTransport = [
+  "rides from friends",
+  " bus (public transit)",
+  " Toyota RAV4",
+  " Tesla Model Y (mom's car)",
+  " Subaru Forester (dad's car)",
+  " walking"
+];
 
-// Functions
+// Create an object for my main ride
+let myMainRide = {
+  make: "Toyota",
+  model: "RAV4",
+  color: "Blue",
+  year: 2007,
+  ownIt: false,
+  currentYear: 2025,
+  // we can define a function within our object (called a method)
+  // that uses the value of the year above (using this.year) in order
+  // to determine the vehicle age
+  age: function() {
+      return this.currentYear - this.year;
+  }
+};
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Kinds of transport I use: ", myTransport, "<br>");
+// The following allows us to write an object to the document
+document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
