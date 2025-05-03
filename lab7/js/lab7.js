690ad3d1b832a8f032fa3ea4d7c24c132a3c19f6
semwong24 <serenemwong@gmail.com>
2025-05-02 17:07:13 -0700

    Lab 7 Updates
    Creation of the Lab 7 files alongside the necessary updates and changes needed to complete the lab's basic tasks.

---
+++ lab7/js/lab7.js
@@ -0,0 +1,29 @@
+// lab7.js - Lab 7: Functions
+// Author: Serene Wong <semwong@ucsc.edu>
+// Date: 5/2/25
+
+// Function to sort the letters of a user's name
+function sortUserName(){
+    //Prompt user for their name
+    let userName = window.prompt("What's your name?");
+    console.log("User NameL ", userName);
+
+    //Give user another try if they cancel or enter nothing
+    if (!userName){
+        userName = window.prompt("You didn't enter a name. Try again:");
+    }
+
+    //If still no text input, exit with message
+    if (!userName){
+        return "You didn't enter a name!";
+    }
+
+    //Convert name to array, sort it, then join back
+    let nameArray = userName.split('');
+    let sortedArray = nameArray.sort();
+    let sortedName = sortedArray.join('');
+
+    return sortedName;
+}
+
+document.writeln("<div class='name-output'>Sorted Name: " + sortUserName() + "</div>");
