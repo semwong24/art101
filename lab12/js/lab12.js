700910b9813bb7c767f7f3429928dabfedaa7062
semwong24 <serenemwong@gmail.com>
2025-05-20 18:24:45 -0700

    Lab 12 Bonus Task
    Changes made here include the bonus task exploration for Lab 12.

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -1,31 +1,98 @@
 /*
-   lab12.js - 
-   Experiments with conditionals.
+   lab12.js - Sorting Hat lab with images and conditionals
+   Requirements: jQuery must be loaded.
 
-   Requirements: jQuery must be loaded for this script to work.
-
-    Author: Serene Wong <semwong@ucsc.edu>
-    Date: 5/20/25
+   Author: Serene Wong <semwong@ucsc.edu>
+   Date: 5/20/25
 */
 
-// Function that assigns a DC group based on the length of the name
-function sortingHat(str){
-    let length = str.length;
-    let mod = length % 4;
-
-    if (mod === 0){
-        return "the Bat Family: Gotham's shadows, filled with vigilantes like Batman, Nightwing, and Batgirl.";
-    } else if (mod === 1){
-        return "the Super Family: Soaring with hope and strength like Superman, Supergirl, and Superboy.";
-    } else if (mod === 2){
-        return "the Amazons: Warriors of wisdom and power like Wonder Woman, Artemis, and Wonder Girl. ";
-    } else if (mod === 3){
-        return "the Speedsters: Quick-witted and fast-paced, from The Flash to Impulse.";
+// Define the houses array with names, descriptions, and image paths
+const houses = [
+    {
+        name: "the Bat Family",
+        description: "Gotham's shadows, filled with vigilantes like Batman, Nightwing, and Batgirl.",
+        image: "img/batfamily.jpg",
+        credit: "Image © DC Comics"
+    },
+    {
+        name: "the Super Family",
+        description: "Soaring with hope and strength like Superman, Supergirl, and Superboy.",
+        image: "img/superfamily.png",
+        credit: "Image © DC Comics"
+    },
+    {
+        name: "the Amazons",
+        description: "Warriors of wisdom and power like Wonder Woman, Artemis, and Wonder Girl.",
+        image: "img/amazons.png",
+        credit: "Image © DC Comics"
+    },
+    {
+        name: "the Speedsters",
+        description: "Quick-witted and fast-paced, from The Flash to Impulse.",
+        image: "img/speedsters.jpg",
+        credit: "Image © DC Comics"
+    },
+    {
+        name: "the Lantern Corps",
+        description: "Emotional warriors wielding powerful rings, from Green Lantern to Larfleeze.",
+        image: "img/lanterncorps.jpg",
+        credit: "Image © DC Comics"
     }
+];
+
+// Function to generate a hash from a string
+function hashCode(str){
+    let hash = 0;
+    for (let i = 0; i < str.length; i++){
+        hash = (hash << 5) - hash + str.charCodeAt(i);
+        hash |= 0;
+    }
+    return Math.abs(hash);
 }
 
-$("#button").click(function(){
-    let name = $("#input").val();
-    let house = sortingHat(name);
-    $("#output").html("<h2>The Sorting Hat has sorted you into <span style='color: gold;'>" + house + "</span></h2>");
+// Function that returns a house based on the length of the name mod houses.length
+function sortingHat(name) {
+    const hash = hashCode(name);
+    const mod = hash % houses.length;
+    console.log(`Hash: ${hash}, mod: ${mod}`);  // Debug log
+    return houses[mod];
+}
+
+$(document).ready(function() {
+    console.log("Document ready, attaching click listener.");
+
+    //Automatically focus the input field when the page loads
+    $("#input").focus();
+
+    $("#button").click(function() {
+        const name = $("#input").val().trim();
+
+        if(name === "") {
+            alert("Please enter your name.");
+            return;
+        }
+
+        console.log(`Input name: ${name}`);
+
+        const house = sortingHat(name);
+
+        // Update output text (with fade=in effect)
+        $("#output").hide().html(`
+            <h2>The Sorting Hat has sorted you into <span style="color: gold;">${house.name}</span>:</h2>
+            <p>${house.description}</p>
+        `).fadeIn();
+
+        // Update image and credit (with fade-in effect)
+        $("#house-image").hide().html(`
+            <img src="${house.image}" alt="${house.name}" class="sorting-image">
+            <div style="font-size: 0.8em; color: lightgray; margin-top: 5px;">${house.credit}</div>
+        `).fadeIn();
+    });
+
+    // Let the Enter key trigger the same action as the button
+    $("#input").keypress(function(event){
+        if (event.which === 13){
+            $("#button").click();
+        }
+    });
 });
